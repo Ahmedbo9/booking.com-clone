531c66c203eba70121e5782c5a7acd29ba5875f6
Ahmedbo9 <ahmed.benothman@live.fr>
2024-03-11 03:23:14 -0400

    adding hero

---
+++ frontend/src/components/hero.tsx
@@ -0,0 +1,17 @@
+const hero = () => {
+    return (
+        <div className="bg-blue-800 pb-16">
+            <div className="container mx-auto flex flex-col gap-2">
+                <h1 className="text-5xl text-white font-bold">
+                    Book for your next trip right now
+                    <p className="text-2xl text-white">
+                        Find the best deals on hotels
+                    </p>
+                </h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default hero
